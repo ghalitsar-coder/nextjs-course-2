0b74cafe1e438815a7fc61a6f970cec523b6fb59
ghalitsar coder <ghalitsardev@gmail.com>
2022-09-17 17:56:34 +0700

    dda

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectMongo = async () =>
-  mongoose.connect(process.env.MONGODB_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  });
+  mongoose.connect(
+    "mongodb+srv://ghalitsar:ember1233@cluster0.paaz9mo.mongodb.net/traveloka?retryWrites=true&w=majority",
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }
+  );
 export const disconnectMongo = async () => mongoose.connection.close();
